d19ea54d8c5c5d009d533ac375603670990286a3
Hans Huebner <hans.huebner@gmail.com>
2022-02-09 06:59:01 +0100

    listp arg in run

--- main.lisp
+++ main.lisp
@@ -9,13 +9,13 @@
 
 (setf *print-length* 40)
 
-(defun assemble ()
-  (multiple-value-bind (memory env) (cl-6502:asm (read-file-into-string *file*) :listp t)
+(defun assemble (listp)
+  (multiple-value-bind (memory env) (cl-6502:asm (read-file-into-string *file*) :listp listp)
     (setf (cl-6502:get-range 0) memory)
     (setf cl-6502:*cpu* (make-instance '6502::symbolic-cpu :env env))))
 
-(defun run ()
-  (assemble)
+(defun run (listp)
+  (assemble listp)
   (cl-6502:execute cl-6502:*cpu*)
   (cl-6502:get-range #x2400))
 
