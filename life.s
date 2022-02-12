258851d2b226f4c44e7a05bdb6b38aaf15392cff
Hans Huebner <hans.huebner@wagestream.com>
2022-02-12 09:41:38 +0100

    prepare for complete field counting

--- life.s
+++ life.s
@@ -5,12 +5,14 @@ tmpa:         .byte 0
 savecarry:    .byte 0               ; bit 0 => carry from previous byte
 inbyte:       .byte 0               ; byte offset to input
 outbyte:      .byte 0               ; byte offset to output
-              
+row:          .byte 0
               .align 2
 outrow:       .word neighbors
 inrow:        .word buf0
               .org  $1000
-start:
+count_neighbors:
+              lda   #24
+              sta   row
 ;;; count neighbors across one screen row
 ;;; inrow => pointer to row of 5 bytes
 countrow:
@@ -71,8 +73,8 @@ get_next_lsb: lda   (inrow),y
               jmp   next_bit
 check_end:    cmp   #7
               beq   next_byte
-              lda   inbyte
-              cmp   #6
+              lda   inbyte          ; check for end of row
+              cmp   #6              ; inbyte is already incremented
               beq   end
               lda   tmpa
               jmp   next_bit
